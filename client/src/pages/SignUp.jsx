01a575316d50513897de086f2910a76f44f06f09
ROHITH R <rohith018.r@gmail.com>
2024-09-03 19:57:05 +0530

    Error fix

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
-import Oauth from '../components/OAuth';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
     const [formData, setFormData] = useState({});
@@ -92,7 +92,7 @@ export default function SignUp() {
                                     <Spinner className='text-center' size='lg' />
                                 ) : ('Sign Up')}
                             </Button>
-                            <Oauth />
+                            <OAuth />
                         </form>
                         <div className="flex gap-2 text-sm mt-5">
                             <span>Have an Account?</span>
